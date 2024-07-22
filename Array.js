267981b198c14bf5b1e2ba5f7ac3e892124468e6
mjavadasadi <asadij313@gmail.com>
2024-07-22 21:37:53 +0330

    Amozesh Array

---
+++ Array.js
@@ -0,0 +1,36 @@
+let ShopingList = [
+  { name: "سیب", price: 3000, quantity: 2 },//0
+  { name: "نان", price: 2000, quantity: 1 },//1
+  { name: "شیر", price: 1000, quantity: 3 },//2
+];
+
+// push
+
+//splice
+
+//forEach
+//map
+
+//filter
+
+ 
+
+ShopingList.push({ name: "تخم مرغ", price: 4000, quantity: 12 });//3
+
+// ShopingList.splice(2,1)
+
+// splice(start,number)
+
+
+
+
+// ShopingList.forEach(item=>console.log('item+++++>',item))
+
+// let productName=ShopingList.map(item=>item.quantity)
+
+// console.log('productName==========>',productName);
+
+
+let expensiveItems =ShopingList.filter(item=>item.price<3000)
+
+console.log('expensiveItems====>',expensiveItems);
