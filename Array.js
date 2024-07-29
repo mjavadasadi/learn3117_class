78e87441c6db0dac951e2afd4ea3884a59a3868f
mjavadasadi <asadij313@gmail.com>
2024-07-29 20:51:28 +0330

    reduce in Array

--- Array.js
+++ Array.js
@@ -1,7 +1,7 @@
 let ShopingList = [
-  { name: "سیب", price: 3000, quantity: 2 },//0
-  { name: "نان", price: 2000, quantity: 1 },//1
-  { name: "شیر", price: 1000, quantity: 3 },//2
+  { name: "سیب", price: 3000, quantity: 2 }, //0
+  { name: "نان", price: 2000, quantity: 1 }, //1
+  { name: "شیر", price: 1000, quantity: 3 }, //2
 ];
 
 // push
@@ -13,16 +13,16 @@ let ShopingList = [
 
 //filter
 
- 
+//reduce
 
-ShopingList.push({ name: "تخم مرغ", price: 4000, quantity: 12 });//3
+let totalCost = ShopingList.reduce((total, item) => 
+  total +(item.price*item.quantity),0);
+
+// ShopingList.push({ name: "تخم مرغ", price: 4000, quantity: 12 });//3
 
 // ShopingList.splice(2,1)
 
 // splice(start,number)
-
-
-
 
 // ShopingList.forEach(item=>console.log('item+++++>',item))
 
@@ -30,7 +30,6 @@ ShopingList.push({ name: "تخم مرغ", price: 4000, quantity: 12 });//3
 
 // console.log('productName==========>',productName);
 
+// let expensiveItems =ShopingList.filter(item=>item.price<3000)
 
-let expensiveItems =ShopingList.filter(item=>item.price<3000)
-
-console.log('expensiveItems====>',expensiveItems);
+console.log("totalCost====>", totalCost);
